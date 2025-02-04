2a1287e881db886893c006f890e7630ce7685f12
재윤 <129163480+jyoons00@users.noreply.github.com>
2025-02-04 17:53:56 +0900

    [level 0] Title: 문자열 출력하기, Time: 203.52 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/README.md
+++ 프로그래머스/0/181952. 문자열 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 출력하기 - 181952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 171.34 ms
+메모리: 72.1 MB, 시간: 203.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 17:48:18
+2025년 02월 04일 17:53:52
 
 ### 문제 설명
 
--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         String str;
-        
         Scanner sc = new Scanner(System.in);
         
         str = sc.nextLine();
-        System.out.println(str);
         
+        if(str.length() >= 1 && str.length() <= 1000000) {
+          System.out.println(str);
+        } else {
+          System.out.println("입력 조건을 만족하지 않습니다.");
+        }
+            
+        sc.close();
         
     }
+    
 }
