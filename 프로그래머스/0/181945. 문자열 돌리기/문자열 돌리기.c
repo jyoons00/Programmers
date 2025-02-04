91bd09deea1a595816013bb3ba0e251a216fb92b
재윤 <129163480+jyoons00@users.noreply.github.com>
2025-02-04 19:52:20 +0900

    [level 0] Title: 문자열 돌리기, Time: 2.67 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.c
@@ -5,7 +5,7 @@ int main(void) {
     char s1[LEN_INPUT];
     scanf("%s", s1);
     
-    for(char *p = s1; *p != '\0'; *p++) {
+    for(char *p = s1; *p != '\0'; p++) {
         
         printf("%c\n", *p);
         
