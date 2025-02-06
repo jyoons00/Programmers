7f12921b4bd13d5262d739450ced8a2df15bd1f5
재윤 <129163480+jyoons00@users.noreply.github.com>
2025-02-06 21:20:38 +0900

    Update N번째 큰 수.java

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-
-        int test = sc.nextInt();
-        int n = 3;
-
+        
+        int test = sc.nextInt(); 
+        
         for (int i = 0; i < test; i++) {
             int[] arr = new int[10];
 
             for (int j = 0; j < 10; j++) {
                 arr[j] = sc.nextInt();
             }
-
+        
             Arrays.sort(arr);
 
-            System.out.println(arr[10-n]);
+            System.out.println(arr[7]);
         }
 
         sc.close();
